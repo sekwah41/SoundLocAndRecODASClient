2ea1af4b6d4e7e6050a66c05af2bc6d11f755e05
Sekwah <sekwah41@gmail.com>
2020-04-11 16:35:24 +0100

    Fixed start script

--- Useful Scripts/start.sh
+++ Useful Scripts/start.sh
@@ -12,6 +12,6 @@ fi
 # Create file from template so it isn't annoying to keep replacing
 sed -e "s/%webip%/$webip/g" template_network_respeaker_4.cfg > network_respeaker_4.cfg
 
-cd ../../bin || exit
+cd ../bin || exit
 
 ./odaslive -c ../config/diss/network_respeaker_4.cfg
