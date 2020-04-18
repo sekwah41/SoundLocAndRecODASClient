875d3dacc49d8ba86585f7e0e0fc5eb950759cc9
Sekwah <sekwah41@gmail.com>
2020-04-18 22:34:28 +0100

    Added config for the matrix voice

---
+++ Useful Scripts/start-matrix.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd ../ || exit
+cd config/diss || exit
+
+webip=$1
+if [ "$webip" == "" ]; then
+  echo "Must specify ip (./start.sh xx.xx.xx.xx)"
+  exit
+fi
+
+# Create file from template so it isn't annoying to keep replacing
+sed -e "s/%webip%/$webip/g" template_network_matrix_voice.cfg > network_matrix_voice.cfg
+
+cd ../../bin || exit
+
+./odaslive -c ../config/diss/matrix_voice.cfg
